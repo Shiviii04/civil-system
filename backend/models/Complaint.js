0d82d869b5b636c560b8be1296cb77eefb2424a3
Shiviii04 <patelshivani4226@gmail.com>
2026-01-17 19:33:46 +0530

    Add files via upload

---
+++ backend/models/Complaint.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const complaintSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Resolved"],
+      default: "Pending",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Complaint", complaintSchema);
